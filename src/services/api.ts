caa4a49a60ca2cf7d561a3e26f3c97d64ac889a3
Lee Ngari <leengari76@gmail.com>
2025-10-21 12:20:52 +0300

    -

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-// TODO: Replace with your Strapi backend URL
-const API_URL = "http://localhost:1337/api";
+const API_URL =
+  import.meta.env.MODE === "production"
+    ? "https://best-trust-b5c8149993.strapiapp.com/api"
+    : "http://localhost:1337/api";
 
 const api = axios.create({
   baseURL: API_URL,
